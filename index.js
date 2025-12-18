5ae8d79a0a90abcc1514bb3d1280306472bf4069
pamkaya-dev <tharakadilshan4433@gmail.com>
2025-12-18 23:15:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ let code = require('./pair');
 
 require('events').EventEmitter.defaultMaxListeners = 500;
 
+app.get('/api/active-bots', (req, res) => {
+  res.json({
+    count: pair.getActiveSockets()
+  })
+})
+
 app.use('/code', code);
 app.use('/pair', async (req, res, next) => {
     res.sendFile(__path + '/pair.html')
